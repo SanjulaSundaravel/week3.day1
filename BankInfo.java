86c32babec2ba63e2ba79388528abeb3a51b1bb1
SanjulaSundaravel <100271402+SanjulaSundaravel@users.noreply.github.com>
2022-03-06 09:35:09 +0530

    Assignment3

---
+++ BankInfo.java
@@ -0,0 +1,14 @@
+package Assignment.week2;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Saving account");
+	}
+	public void fixed() {
+		System.out.println("Fixed amount");
+	}
+	public void deposit() {
+		System.out.println("Amount deposited - Parent");
+	}
+
+}
